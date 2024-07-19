f5affa747f96011bec16a89cdf00a2ac7c5e0cf8
Ryosei Goto <swear1208a13@gmail.com>
2024-07-19 16:43:41 +0900

    Create bool loader

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+EDK2_DIR=~/edk2
+
+cd $EDK2_DIR
+source edksetup.sh
+build
+
---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+EDK2_DIR=~/edk2
+BOOTLOADER_DIR=$EDK2_DIR/Build/MikanLoaderX64/DEBUG_CLANG38/X64/MikanLoaderPkg/Loader/DEBUG/Loader.efi
+OSBOOK_DIR=~/osbook
+
+$OSBOOK_DIR/devenv/run_qemu.sh $BOOTLOADER_DIR
+

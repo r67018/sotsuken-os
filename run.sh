514060d6045380b59e48dfc6d279db2abcf854c8
Ryosei Goto <swear1208a13@gmail.com>
2024-10-07 13:37:50 +0900

    Add kernel file

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 EDK2_DIR=~/edk2
 BOOTLOADER_DIR=$EDK2_DIR/Build/MikanLoaderX64/DEBUG_CLANG38/X64/MikanLoaderPkg/Loader/DEBUG/Loader.efi
 OSBOOK_DIR=~/osbook
+KERNEL_FILE=./kernel/kernel.elf
 
-$OSBOOK_DIR/devenv/run_qemu.sh $BOOTLOADER_DIR
+$OSBOOK_DIR/devenv/run_qemu.sh $BOOTLOADER_DIR $KERNEL_FILE
 

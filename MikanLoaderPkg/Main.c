fc63982839e0e2432098ebff268afc975937d952
Ryosei Goto <swear1208a13@gmail.com>
2024-12-23 14:53:28 +0900

    Add ELF file parsing and dynamic kernel loading functionality
    Introduced ELF header and program header parsing to support loading ELF files into memory dynamically. This includes calculating memory ranges for ELF segments, copying LOAD segments into allocated memory, and freeing temporary buffers. Enhanced kernel loading process to handle ELF-formatted binaries efficiently.

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -3,12 +3,14 @@
 #include  <Library/UefiBootServicesTableLib.h>
 #include  <Library/PrintLib.h>
 #include  <Library/MemoryAllocationLib.h>
+#include  <Library/BaseMemoryLib.h>
 #include  <Protocol/LoadedImage.h>
 #include  <Protocol/SimpleFileSystem.h>
 #include  <Protocol/DiskIo2.h>
 #include  <Protocol/BlockIo.h>
 #include  <Guid/FileInfo.h>
 
+#include "elf.hpp"
 #include "frame_buffer_config.hpp"
 
 struct MemoryMap {
@@ -152,6 +154,30 @@ const CHAR16* GetPixelFormatUnicode(EFI_GRAPHICS_PIXEL_FORMAT fmt) {
   }
 }
 
+void CalcLoadAddressRange(Elf64_Ehdr* ehdr, UINT64* first, UINT64* last) {
+    Elf64_Phdr* phdr = (Elf64_Phdr*)((UINT64)ehdr + ehdr->e_phoff);
+    *first = MAX_UINT64;
+    *last = 0;
+    for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i) {
+        if (phdr[i].p_type != PT_LOAD) continue;
+        *first = MIN(*first, phdr[i].p_vaddr);
+        *last = MAX(*last, phdr[i].p_vaddr + phdr[i].p_memsz);
+    }
+}
+
+void CopyLoadSegments(Elf64_Ehdr* ehdr) {
+    Elf64_Phdr* phdr = (Elf64_Phdr*)((UINT64)ehdr + ehdr->e_phoff);
+    for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i) {
+        if (phdr[i].p_type != PT_LOAD) continue;
+
+        UINT64 segm_in_file = (UINT64)ehdr + phdr[i].p_offset;
+        CopyMem((VOID*)phdr[i].p_vaddr, (VOID*)segm_in_file, phdr[i].p_filesz);
+
+        UINTN remain_bytes = phdr[i].p_memsz - phdr[i].p_filesz;
+        SetMem((VOID*)(phdr[i].p_vaddr + phdr[i].p_filesz), remain_bytes, 0);
+    }
+}
+
 void Halt(void) {
     while(1) __asm__("hlt");
 }
@@ -204,17 +230,43 @@ EFI_STATUS EFIAPI UefiMain(
     EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
     UINTN kernel_file_size = file_info->FileSize;
 
+    // カーネルを一時領域に読み込む
+    VOID* kernel_buffer;
     EFI_STATUS status;
-    EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
-    status = gBS->AllocatePages(
-        AllocateAddress, EfiLoaderData,
-        (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr);
+    status = gBS->AllocatePool(EfiLoaderData, kernel_file_size, &kernel_buffer);
     if (EFI_ERROR(status)) {
-        Print(L"Failed to allocate pages: %r", status);
+        Print(L"Failed to allocate pool: %r\n", status);
         Halt();
     }
-    kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
-    Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
+    status = kernel_file->Read(kernel_file, &kernel_file_size, kernel_buffer);
+    if (EFI_ERROR(status)) {
+        Print(L"Error: %r", status);
+        Halt();
+    }
+
+    // カーネルのプログラムヘッダの先頭と末尾のアドレスを計算
+    Elf64_Ehdr* kernel_ehdr = (Elf64_Ehdr*)kernel_buffer;
+    UINT64 kernel_first_addr, kernel_last_addr;
+    CalcLoadAddressRange(kernel_ehdr, &kernel_first_addr, &kernel_last_addr);
+
+    // 先頭と末尾のアドレスから割り当てるメモリのページ数を計算する
+    UINTN num_pages = (kernel_last_addr - kernel_first_addr + 0xfff) / 0x1000;
+    status = gBS->AllocatePages(AllocateAddress, EfiLoaderData, num_pages, &kernel_first_addr);
+    if (EFI_ERROR(status)) {
+        Print(L"Failed to allocate pages: %r\n", status);
+        Halt();
+    }
+
+    // LOADセグメントをメモリにコピーする
+    CopyLoadSegments(kernel_ehdr);
+    Print(L"Kernel: %0x%0lx\n", kernel_first_addr, kernel_last_addr);
+
+    // 一時領域のメモリを開放する
+    status = gBS->FreePool(kernel_buffer);
+    if (EFI_ERROR(status)) {
+        Print(L"Failed to free pool: %r\n", status);
+        Halt();
+    }
 
     // ブートサービスを停止する
     status = gBS->ExitBootServices(image_handle, memmap.map_key);
@@ -253,7 +305,7 @@ EFI_STATUS EFIAPI UefiMain(
 
     // カーネルを呼び出す
     // EFIのエントリーポイントは先頭から24バイトの位置に8バイトの整数として格納されている
-    UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
+    UINT64 entry_addr = *(UINT64*)(kernel_first_addr + 24);
 
     typedef void EntryPointType(struct FrameBufferConfig*);
     EntryPointType* entry_point = (EntryPointType*)entry_addr;
--- MikanLoaderPkg/frame_buffer_config.hpp
+++ MikanLoaderPkg/frame_buffer_config.hpp
@@ -14,3 +14,12 @@ struct FrameBufferConfig {
     uint32_t vertical_resolution;
     enum PixelFormat pixel_format;
 };
+
+#define PT_NULL    0
+#define PT_LOAD    1
+#define PT_DYNAMIC 2
+#define PT_INTERP  3
+#define PT_NOTE    4
+#define PT_SHLIB   5
+#define PT_PHDR    6
+#define PT_TLS     7
